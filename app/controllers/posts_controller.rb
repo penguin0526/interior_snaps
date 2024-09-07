1d8a671617d8d142adc405f869c2ea2a6d5fe909
KokiOkuda <165885793+penguin0526@users.noreply.github.com>
2024-09-07 01:46:06 +0000

    [Start]post_controll

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,42 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to user_path(@user.id)
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post.id)
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :post_image, :body)
   end
 end
