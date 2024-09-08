b43784cedb6a3fccb7ec335ab6343d36ae393480
KokiOkuda <165885793+penguin0526@users.noreply.github.com>
2024-09-08 17:35:00 +0000

    [Update]post,new image+index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to user_path(@user.id)
+    redirect_to post_path(@post.id)
   end
 
   def edit
@@ -37,6 +37,12 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :post_image, :body)
+    params.require(:post).permit(:title, :body, :user_id, :images => [])
+  end
+
+  def correct_user
+    @post = Post.find(params[:id])
+    @user = @post.user
+    redirect_to(posts_path) unless @user == current_user
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,11 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  belongs_to :users
-  belongs_to :genres
-  has_one_attached :image
+  belongs_to :user
+  belongs_to :genre
+  has_many_attached :images
+
+  validates :title, presence: true
+  validates :images, presence: true
+  validates :body, presence: true
 end
