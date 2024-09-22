f47f0031739b274b0a8ea21b7ba8626324b7c551
KokiOkuda <165885793+penguin0526@users.noreply.github.com>
2024-09-22 17:09:15 +0000

    [Update]comment view

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
 
   resources :posts do
-    resources :post_comments, only: [:create]
+    resources :comments, only: [:create]
     resources :favorites, only: [:create, :destroy]
   end
 
