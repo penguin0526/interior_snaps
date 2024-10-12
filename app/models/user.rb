245029b31d2b63b913e7c62f21661c9193763073
KokiOkuda <165885793+penguin0526@users.noreply.github.com>
2024-10-12 16:03:22 +0000

    [Update]model.user.rb add favorites.dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favorited_posts, through: :favorites, source: :post
 
   validates:name,
