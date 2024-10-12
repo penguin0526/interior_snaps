5cf2d31c6bed99c3328c64736eb723acdc5451e6
KokiOkuda <165885793+penguin0526@users.noreply.github.com>
2024-10-12 13:12:03 +0000

    [Update]seed.rb admin email,pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.find_or_create_by!(email: ENV['admin@example.pom']) do |admin|
-  admin.password = ENV['password']
+Admin.find_or_create_by!(email: 'admin@example.pom') do |admin|
+  admin.password = 'password'
 end
 
 penguin = User.find_or_create_by!(email: "penguin@example.com") do |user|
